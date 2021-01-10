d6c1ec7ae43a77c954afdde23602f36ce78c719f
electriclantern <quewon.chin@gmail.com>
2021-01-11 00:00:12 +0900

    .

--- scenes.js
+++ scenes.js
@@ -3,7 +3,7 @@ var SCENE = {
 		sceneobject: {
 			name: "an oasis",
 			line: "around you is a small oasis. it's a lonely pond and a small [bridge].",
-			kitty: [1, `<a onclick="t(DIALOGUE.fisher)">fisher</a>'s gone and you've got nowhere to go. you spend your days wandering aimlessly, following the slightest whispers.<br /><br />the ghosts, they led you here. now there's nothing. only you, this heat, and the water running under your feet.`, ""]
+			kitty: [1, `<a onclick="t(DIALOGUE.fisher)">fisher</a>'s gone, the team's broken up, and you've got nowhere to go. you spend your days wandering aimlessly, following the slightest whispers.<br /><br />the ghosts, they led you here. now there's nothing. only you, this heat, and the water running under your feet.`, ""]
 		},
 		exits: [
 			{
@@ -40,7 +40,7 @@ var SCENE = {
 		ghosts: [],
 		exits: [
 			{
-				line: "on one side of this wall, the vast [desert].",
+				line: "on one side of this wall, the [desert].",
 				destination: "desert",
 			},
 			{
@@ -54,7 +54,7 @@ var SCENE = {
 	wall_golem: {
 		sceneobject: {
 			name: "wall golem",
-			line: "the wall is a [golem].<br /><br />HEY, it says, WATCH WHERE YOU'RE GOING, LADY.<br /><br /><i>that's it for now...! more to come</i>",
+			line: `this wall is a [golem].<br /><br />HEY, it says, WATCH WHERE YOU'RE GOING, LADY.<br /><br /><i>that's it for now...! more to come</i>`,
 			kitty: [1, `should you be surprised you didn't realize sooner? you hear ghosts, not golems, even if they are just ghosts in clay costumes. plus, you've... never seen a golem so big. it's scary big.`, ""]
 		},
 		visited: false,
